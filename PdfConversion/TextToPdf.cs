844411887a5d1d579fc2b00614618981feca993c
Rahul Dey <rahuldey832000@gmail.com>
2024-02-27 10:28:48 +0530

    Add Swagger UI Documentation

--- PdfConversion/TextToPdf.cs
+++ PdfConversion/TextToPdf.cs
@@ -1,7 +1,10 @@
+using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace TextToPdf;
 
@@ -10,9 +13,15 @@ public class TextToPdf(ILogger<TextToPdf> logger)
     private readonly ILogger<TextToPdf> _logger = logger;
 
     [Function("TextToPdf")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    [OpenApiOperation()]
+    [OpenApiParameter(name: "Name", Required = true, In = ParameterLocation.Query, Type = typeof(string))]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/text", bodyType: typeof(string))]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+
+        var name = req.Query["name"];
+
+        return new OkObjectResult($"Hello {name}, Welcome to Azure TextToPdf Functions!");
     }
 }
