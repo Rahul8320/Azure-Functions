d49fbbe0dccf3979f96664ee4e74c4c9f07b3fe7
Rahul Dey <rahuldey832000@gmail.com>
2024-02-27 10:08:14 +0530

    Create Pdf Conversion Function App

---
+++ PdfConversion/TextToPdf.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace TextToPdf;
+
+public class TextToPdf(ILogger<TextToPdf> logger)
+{
+    private readonly ILogger<TextToPdf> _logger = logger;
+
+    [Function("TextToPdf")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
