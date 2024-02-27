9a59e23d16344cd811a88c72d0dfd987c4d508e6
Rahul Dey <rahuldey832000@gmail.com>
2024-02-27 11:15:55 +0530

    Create text to pdf function

--- PdfConversion/TextToPdf.cs
+++ PdfConversion/TextToPdf.cs
@@ -5,6 +5,8 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
 
 namespace TextToPdf;
 
@@ -14,14 +16,43 @@ public class TextToPdf(ILogger<TextToPdf> logger)
 
     [Function("TextToPdf")]
     [OpenApiOperation()]
-    [OpenApiParameter(name: "Name", Required = true, In = ParameterLocation.Query, Type = typeof(string))]
-    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/text", bodyType: typeof(string))]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+    [OpenApiParameter(name: "Text", Required = true, In = ParameterLocation.Query, Type = typeof(string))]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/pdf", bodyType: typeof(string))]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/text", bodyType: typeof(string))]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        var name = req.Query["name"];
+        // Get the query parameter
+        var text = req.Query["text"];
 
-        return new OkObjectResult($"Hello {name}, Welcome to Azure TextToPdf Functions!");
+        // check if text is exists or not
+        if (string.IsNullOrEmpty(text))
+        {
+            return new BadRequestObjectResult("The required query parameter text cannot be null or empty!");
+        }
+
+        // Create the pdf document
+        using PdfDocument document = new();
+        PdfPage page = document.Pages.Add();
+
+        PdfGraphics graphics = page.Graphics;
+
+        graphics.DrawString(text!,
+            new PdfStandardFont(PdfFontFamily.Helvetica, 20),
+            PdfBrushes.Black,
+            new Syncfusion.Drawing.PointF(0, 0));
+
+        using MemoryStream outputStream = new();
+        document.Save(outputStream);
+        outputStream.Position = 0;
+        document.Dispose();
+
+        string contentType = "application/pdf";
+        string fileName = "document.pdf";
+
+        req.HttpContext.Response.Headers.Append("Content-Disposition", $"attachment;{fileName}");
+
+        return new FileContentResult(outputStream.ToArray(), contentType);
     }
 }
